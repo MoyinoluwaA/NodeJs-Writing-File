782fd385ad420845de71d9957f5a6f8ce68ee05a
Moyinoluwa <dayoh14@gmail.com>
2021-04-17 16:34:16 +0100

    dynamically created result folder

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
 
+try {
+    if (!fs.existsSync('result')) {
+      fs.mkdirSync('result')
+    }
+} catch (err) {
+    console.error(err)
+}
+
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(json => {
